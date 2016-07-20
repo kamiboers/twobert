e4faba732d5311f50338588e35cf68030e87af17
Kami Boers <kamiboers@gmail.com>
2016-07-20 01:18:24 -0600

    socket.io added, first server connection

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static('static'));
 
-app.set('port', process.env.PORT || 6060);
+// app.set('port', process.env.PORT || 6060);
 app.locals.title = 'Q*BERT';
+
+
+if (!module.parent) {
+  // app.listen(app.get('port'), () => {
+  //   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  // });
+  server.listen(6060)
+}
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-if (!module.parent) {
-  app.listen(app.get('port'), () => {
-    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-  });
-}
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+
+io.on('connection', function(socket) {
+    console.log('New connection');
+
+    socket.on('message', function(msg) {
+        console.log('Got message from client: ' + msg);
+    });
+});
 
 module.exports = app;
